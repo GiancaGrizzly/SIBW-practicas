1045d6ec654c36924f9c63700b1694249247bace
GiancaGrizzly <carloscorteza5@gmail.com>
2022-03-28 21:11:33 +0200

    feat: comprobador de email v1 -- practica2

--- static/js/producto.js
+++ static/js/producto.js
@@ -1,16 +1,31 @@
+/* direccion = usuario + @ + servidor + dominio
+ * usuario y servidor
+ *   comienza por al menos una letra o número (\w+)
+ *   seguida de 0 o más combinaciones de punto o guión y letras o números ([\.-]?w+)*
+ * dominio
+ *   comienza por punto \.
+ *   le siguen 2, 3 o 4 letras \w{2,3,4}
+ *   además, puede haber varios dominios (\.\w{2,3,4})+
+ * 
+ * /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3,4})+$/
+ */
+
 function openForm()
 {
-    document.getElementById("id-container-formulario").style.display = "block";
+    document.getElementById("id-container-form").style.display = "block";
     document.getElementById("boton").style.display = "none";
 }
 
 function closeForm()
 {
-    document.getElementById("id-container-formulario").style.display = "none";
+    document.getElementById("id-container-form").style.display = "none";
     document.getElementById("boton").style.display = "block";
 }
 
 function comprobarEmail(email)
 {
-    document.getElementById("id-email").style.backgroundColor = "red";
+    if (!/^\w+@\w+(\.\w{2,4})+$/.test(email)) {
+        alert("Dirección de correo electrónico inválida");
+        document.getElementById("id-email").value = "";
+    }
 }
