b0ce8878c68279abed267668e73c33c726bc2083
GiancaGrizzly <carloscorteza5@gmail.com>
2022-03-28 17:43:08 +0200

    fix: corrijo comportamiento del boton Cancelar en el formulario -- practica2

--- static/js/producto.js
+++ static/js/producto.js
@@ -1,12 +1,16 @@
 function openForm()
 {
-    document.getElementById("formulario").style.display = "block";
+    document.getElementById("id-container-formulario").style.display = "block";
     document.getElementById("boton").style.display = "none";
 }
 
 function closeForm()
 {
-    document.getElementById("formulario").style.display = "none";
+    document.getElementById("id-container-formulario").style.display = "none";
     document.getElementById("boton").style.display = "block";
 }
 
+function comprobarEmail(email)
+{
+    document.getElementById("id-email").style.backgroundColor = "red";
+}
