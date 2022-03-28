63f47c9090e52d431f3a1f1715c643a1d284c7fb
GiancaGrizzly <carloscorteza5@gmail.com>
2022-03-28 16:53:50 +0200

    feat: añado toda la apariencia del formulario -- practica2

---
+++ static/js/producto.js
@@ -0,0 +1,12 @@
+function openForm()
+{
+    document.getElementById("formulario").style.display = "block";
+    document.getElementById("boton").style.display = "none";
+}
+
+function closeForm()
+{
+    document.getElementById("formulario").style.display = "none";
+    document.getElementById("boton").style.display = "block";
+}
+
