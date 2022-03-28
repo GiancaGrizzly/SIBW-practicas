cb2998b9a436f5ffbad49c44ded2d6f76e03b2eb
GiancaGrizzly <carloscorteza5@gmail.com>
2022-03-28 21:29:48 +0200

    feat: comprobador de email v2 -- practica2

--- static/js/producto.js
+++ static/js/producto.js
@@ -1,13 +1,13 @@
-/* direccion = usuario + @ + servidor + dominio
- * usuario y servidor
- *   comienza por al menos una letra o número (\w+)
- *   seguida de 0 o más combinaciones de punto o guión y letras o números ([\.-]?w+)*
+/* direccion_email = usuario + @ + servidor + dominio
+ * usuario
+ *   comienza por al menos una letra o número \w+
+ *   seguida de 0 o más combinaciones de punto o guión y letras o números ((\.|-)\w+)*
+ * servidor
+ *   al menos una letra o número \w+
  * dominio
  *   comienza por punto \.
  *   le siguen 2, 3 o 4 letras \w{2,3,4}
  *   además, puede haber varios dominios (\.\w{2,3,4})+
- * 
- * /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3,4})+$/
  */
 
 function openForm()
@@ -24,7 +24,7 @@ function closeForm()
 
 function comprobarEmail(email)
 {
-    if (!/^\w+@\w+(\.\w{2,4})+$/.test(email)) {
+    if (!/^\w+((\.|-)\w+)*@\w+(\.\w{2,4})+$/.test(email)) {
         alert("Dirección de correo electrónico inválida");
         document.getElementById("id-email").value = "";
     }
