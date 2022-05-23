4df67b346b2a0251ac9682112a5879457b371805
GiancaGrizzly <carloscorteza5@gmail.com>
2022-05-23 16:09:21 +0200

    creo de nuevo la tabla usuarios -- se hace log in/log out y se muestra la informacion del usuario en la pagina de perfil_usuario.html

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -10,10 +10,7 @@
 
     session_start();
 
-    if (isset($_SESSION['nombre'])) {
-
-        $variablesTwig['usuario'] = get_usuario($_SESSION['nombre']);
-    }
+    $variablesTwig['usuario'] = get_usuario($_SESSION['nombre']);
 
     echo $twig->render('perfil_usuario.html', $variablesTwig);
 
